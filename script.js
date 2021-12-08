6ec4a5b9250aa94e00871830b33458e5d208052a
Abrar Ali <91776475+abrarali-official@users.noreply.github.com>
2021-12-08 20:29:57 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-document.getElementById("dom").innerHTML = 5 + 6;
+//                                       1st program
+// program to find the largest among two numbers
+let num = max(150, 50)
+    // console.log(num)
+document.getElementById("dom").innerHTML = num;
+
+function max(num1, num2) {
+    if (num1 > num2)
+        return num1
+    return num2
+}
+//                                         2nd program
+// program to find the largest among three numbers
+
+// if you to take input from the user then use these three parsefloats
+var num1 = 200 //parseFloat(prompt("Enter first number: "));
+var num2 = 150 //parseFloat(prompt("Enter second number: "));
+var num3 = 55 //parseFloat(prompt("Enter third number: "));
+let largest;
+
+// check the condition
+if (num1 >= num2 && num1 >= num3) {
+    largest = num1;
+} else if (num2 >= num1 && num2 >= num3) {
+    largest = num2;
+} else {
+    largest = num3;
+}
+document.getElementById("demo").innerHTML = largest; // display the result
+
+//                                       3rd program
